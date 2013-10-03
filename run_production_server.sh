95be427cd3a1c7cd72bebc1ab4fddeb6906c6f60
Ondrej Brinkel <info@anzui.de>
2013-10-03 20:18:37 +0200

    npm update added node_modules to repo

--- run_production_server.sh
+++ run_production_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 datestamp=$(date +%d-%m-%Y-%H.%M.%S)
-NODE_ENV=production nohup node app.js > log/output_${datestamp}.log
+NODE_ENV=production node app.js > log/output_${datestamp}.log &
---
+++ stop_forever.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+forever stop app.js
