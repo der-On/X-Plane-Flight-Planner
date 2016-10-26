d52dcceafd0ce37fd87778feed1d0f5a20bcdd25
Ondrej Brinkel <info@anzui.de>
2016-10-26 15:51:10 +0200

    now really fixed missing Piwik server now logs where it listens

--- tasks/serve.js
+++ tasks/serve.js
@@ -4,9 +4,11 @@ var path = require('path');
 var distPath = path.join(__dirname, '../dist');
 var express = require('express');
 var app = express();
-
+var port = process.env.PORT || 8080;
 app
   .use(express.static(distPath))
-  .listen(process.env.PORT || 8080);
+  .listen(port);
+
+console.log('Listening on http://localhost:' + port);
 
 module.exports = app;
